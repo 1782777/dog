28d85897a715839408d51ec49c78e3d19a0a6bb4
zx <771279043@qq.com>
2020-05-14 11:11:32 +0800

    test

---
+++ http_test.py
@@ -0,0 +1,18 @@
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+data = {'result': 'this is a http server test'}
+host = ('182.92.114.73', 8080)
+
+class Resquest(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Resquest)
+    print("Starting http server, listen at: %s:%s" % host)
+    server.serve_forever()
