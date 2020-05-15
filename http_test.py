77fc2730631ab24559cbc136db09ef67f04fb207
zx <771279043@qq.com>
2020-05-15 15:53:51 +0800

    test

--- http_test.py
+++ http_test.py
@@ -1,9 +1,10 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import cgi
 
 data = {'result': 'this is a http server test'}
-host = ('172.0.0.1', 8080)
+host = ('127.0.0.1', 8080)
 
 class Resquest(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -13,9 +14,19 @@ class Resquest(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(data).encode())
         
     def do_POST(self):
-        data = self.rfile.read(int(self.headers["content-length"]))
+        #data = self.rfile.read(int(self.headers["content-length"]))
+        form =cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={
+                'REQUEST_METHOD':'POST',
+                'CONTENT_TYPE':self.headers['Content-Type'],
+            }
+        )
         for field in form.keys():
-            print(field)
+            field_items = form[field]
+            print(field_items.filename)
+            print(field_items.value)
 
 if __name__ == '__main__':
     server = HTTPServer(host, Resquest)
--- post.py
+++ post.py
@@ -1,10 +1,8 @@
 import requests
-import cv2
-
-# url ="http://182.92.114.73:8080/"
-url ="http://127.0.0.1:8080/"
-
-data ='{"key":"value"}'
-
-res = requests.post(url=url,data=data)
-print res.text
+url = "http://127.0.0.1:8080"
+path = "./0926.png"
+print (path)
+files = {'file': open(path, 'rb')}
+r = requests.post(url, files=files)
+print (r.url)
+print (r.text)
